58f0bcff795c13e57438cb47f7af164ed73fa5b5
Mehdi Rafenne <renaissance@farem.tech>
2021-01-02 23:56:57 +0100

    design: UploaderSuccess designes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Uploader from './components/Uploader';
 import UploaderLoading from './components/UploaderLoading';
+import UploaderSuccess from './components/UploaderSuccess';
 
 function App() {
   return (
     <div className="App">
       {/*<Uploader />*/}
-      <UploaderLoading />
+      <UploaderSuccess />
     </div>
   );
 }
