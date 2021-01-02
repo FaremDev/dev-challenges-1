7f2dd50b7010b9e83bf231698ee2f8e02c9701aa
Mehdi Rafenne <renaissance@farem.tech>
2021-01-02 23:22:33 +0100

    design: Uploader component designed

---
+++ src/components/Uploader.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled from 'styled-components' 
+
+const UploaderContainer = styled.div`
+    display: block;
+    position: absolute;
+	top:0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	margin: auto;
+    background-color:white;
+    color:#333333;
+    border-radius: 20px;
+    box-shadow: 4px 10px 10px lightgray;
+    width: 500px;
+    height: 600px;
+    padding: 20px 5px;
+`
+
+const Title = styled.div`
+    color: black;
+    font-size: 25px;
+    margin: 15px;
+`
+
+const Legend = styled.div`
+    color: gray;
+    font-style: italic;
+    margin: 15px;
+`
+
+const DragDropZone = styled.div`
+    margin: 15px auto;
+    background-color: gray;
+    width: 400px;
+    height: 400px;
+`
+
+const Button = styled.a`
+  display: inline-block;
+  border-radius: 7px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 10rem;
+  background: blue;
+  color: white;
+  border: 2px solid blue;
+`
+
+const Uploader = () => {
+    return (
+        <UploaderContainer>
+            <div className="instructions">
+                <Title>Upload your image</Title>
+                <Legend>File should be JPG, PNG...</Legend>
+            </div>  
+            <DragDropZone />  
+            <div className="button-zone"> 
+                <Legend>or</Legend>
+                <Button>
+                    Choose a file...
+                </Button>
+            </div> 
+        </UploaderContainer>
+    )
+}
+
+export default Uploader;
